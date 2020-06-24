b52197abe5b68a49ef4e666e4fddb1eedb1a3e09
Janne <janne@Mac-MacBook-Pro.local>
2020-06-24 14:35:14 +0300

    merge to BIG main

--- main.c
+++ main.c
@@ -2,23 +2,15 @@
 #include <strings.h>
 #include <string.h>
 
-void	ft_bzero(void *s, size_t n);
 char	*ft_strcpy(char *dst, const char *src);
+void	ft_strclr(char *s);
 
 int	main(void)
 {
 	char source[] = "Keke Orava";
 	char destination[11];
-//	const char *restrict s1;
-//	const char *restrict s2;
 
-//	s1 = "keke";
-//	s2 = "orava";
-	printf("ft_strcpy output: %s\n", ft_strcpy(destination, source));
-	printf("strcpy output is: %s\n", strcpy(destination, source));
-//	printf("ft_bzero output: \n", ft_bzero(argv[1], 4));
-//	printf("bzero output is: %s\n", bzero(str, 4));
-//	strcat(s1, s2);
-//	printf("%s\n", s1);
+//	ft_strclr(source);
+	printf("ft_strcpy output: %s\n", ft_strcpy(destination, source)) && printf("strcpy output is: %s\n", strcpy(destination, source));
 	return (0);
 }
