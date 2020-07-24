cb811ea6de3b829587829ef7c9a83044dbaa0550
Janne Raty <jraty@c1r6p13.hive.fi>
2020-07-24 10:23:57 +0300

    free added, but still needs work

--- ft_strsplit.c
+++ ft_strsplit.c
@@ -6,7 +6,7 @@
 /*   By: jraty <jraty@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/25 15:01:03 by jraty             #+#    #+#             */
-/*   Updated: 2020/07/01 09:47:49 by jraty            ###   ########.fr       */
+/*   Updated: 2020/07/24 09:53:56 by jraty            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,15 +20,11 @@ static int	ft_count_words(const char *s, char c)
 	while (*s)
 	{
 		while (*s == c)
-		{
 			++s;
-		}
 		if (*s)
 			++i;
 		while (*s != c && *s)
-		{
 			++s;
-		}
 	}
 	return (i);
 }
@@ -39,13 +35,9 @@ static int	ft_count_len(const char *s, char c)
 
 	i = 0;
 	while (s[i] == c)
-	{
 		++s;
-	}
 	while (s[i] != c && s[i])
-	{
 		++i;
-	}
 	return (i);
 }
 
@@ -62,14 +54,14 @@ char		**ft_strsplit(char const *s, char c)
 	while (words > 0)
 	{
 		while (*s == c)
-		{
 			++s;
+		if (!(str[i] = ft_strsub(s, 0, ft_count_len(s, c))))
+		{
+			free(str);
+			return (NULL);
 		}
-		str[i] = ft_strsub(s, 0, ft_count_len(s, c));
 		while (*s && *s != c)
-		{
 			++s;
-		}
 		++i;
 		--words;
 	}
